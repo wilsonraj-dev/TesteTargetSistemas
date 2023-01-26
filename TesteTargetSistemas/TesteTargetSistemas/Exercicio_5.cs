45a0ebdcbb13ebf679021a92a0a4b496e95f8640
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-25 23:17:03 -0300

    Exercício 5

---
+++ TesteTargetSistemas/TesteTargetSistemas/Exercicio_5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TesteTargetSistemas
+{
+    class Exercicio_5
+    {
+        static void Main(string[] args)
+        {
+            /* 5) Escreva um programa que inverta os caracteres de um string.
+                  
+                  IMPORTANTE:
+                  a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode 
+                  ser previamente definida no código;
+                  b) Evite usar funções prontas, como, por exemplo, reverse; */
+
+            //Usando while
+            string stringParaReverter = Console.ReadLine();
+            string stringReversa = "";
+            int length = stringParaReverter.Length - 1;
+
+            while (length >= 0)
+            {
+                stringReversa += stringParaReverter[length];
+                length--;
+            }
+            Console.WriteLine(stringReversa);
+
+            //Usando for
+            char[] stringArray = stringParaReverter.ToCharArray();
+            stringReversa = String.Empty;
+
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                stringReversa += stringArray[i];
+            }
+            Console.WriteLine(stringReversa);
+        }
+    }
+}
