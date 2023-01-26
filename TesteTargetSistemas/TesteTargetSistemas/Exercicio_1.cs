a7985fb2c91616bfd118e1735c5f0598059060d6
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-25 23:02:29 -0300

    Exercício 1

---
+++ TesteTargetSistemas/TesteTargetSistemas/Exercicio_1.cs
@@ -0,0 +1,19 @@
+﻿namespace TesteTargetSistemas
+{
+    class Exercicio_1
+    {
+        /* 1) Observe o trecho de código abaixo:
+
+              int INDICE = 13, SOMA = 0, K = 0;
+
+              enquanto K < INDICE faça
+              {
+              K = K + 1;
+              SOMA = SOMA + K;
+              }
+
+              imprimir(SOMA);
+
+              Ao final do processamento, qual será o valor da variável SOMA? O VALOR DA VARIÁVEL SOMA SERÁ 91 */
+    }
+}
