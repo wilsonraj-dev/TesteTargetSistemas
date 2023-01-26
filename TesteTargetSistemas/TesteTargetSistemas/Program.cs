46e90507c2c11454a3b78aa4e2970f3cb6ce97ba
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-01-25 22:59:55 -0300

    Criação do projeto

---
+++ TesteTargetSistemas/TesteTargetSistemas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteTargetSistemas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
